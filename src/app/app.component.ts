3d6c2135307648b3101aa941d7ac2609b5a4a31f
Gus <151650535+GusFDLPBarcelona@users.noreply.github.com>
2024-10-16 21:39:12 +0200

    Pequeños ajustes de estructura. Añado la librería tailwind con daisyUi para los estilos.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from './componentes/home/home.component';
+import { HeaderComponent } from "./componentes/header/header.component";
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HomeComponent],
+  imports: [RouterOutlet, HomeComponent, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
